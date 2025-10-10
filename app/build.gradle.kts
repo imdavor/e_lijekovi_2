1ac1b905069ecb23ea6e4a2c4b70f89807c2992f
imdavor <davor.kos@gmail.com>
2025-10-10 21:13:50 +0200

    dodana je konfiguracija upravitelja uređaja, ažurirana je klasa podataka Lijek za podršku višestrukih vremena unosa i poboljšane su teme boja s Material Designom 3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.kotlinx.serialization.json)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
